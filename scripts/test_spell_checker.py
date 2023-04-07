33b95d8ff3d1e735b48bb8b4932fefa5868b8dfb
pioui <piyilozou@gmail.com>
2023-04-08 00:21:27 +0300

    lvw, evw

--- scripts/test_spell_checker.py
+++ scripts/test_spell_checker.py
@@ -29,12 +29,8 @@ with open(test_file, "r") as f:
 
 # For every mispelled word run the spellchecker S.fst and print the prediction
 for word in word_table.keys():
-    print('-----------------------------------')
     print('{:20s} {:20s}'.format('Correct word:', word))
-    print('{:20s} {:20s}'.format('Input word ', 'Prediction'))
-    print('-----------------------------------')
     for wrong_word in word_table[word]:
         prediction = run_cmd(f'bash scripts/predict.sh {spell_checker_fst} {wrong_word}')
-        # print(f' {wrong_word} --> {prediction}')
-        print('{:20s} {:20s}'.format(wrong_word, prediction))
+        print('{:20s} --> {:20s}'.format(wrong_word, prediction))
     print('-----------------------------------')
